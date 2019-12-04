944273b0ddeb418ff9e688c065dcfd5293da25cb
Rik <Rik@workstation.com>
2019-12-04 11:41:45 +0100

    Added order example

---
+++ order_example/scripts/send_order.py
@@ -0,0 +1,14 @@
+import rospy
+from my_msgs.msg import Order
+
+rospy.init_node("send_order_example")
+order_publisher = rospy.Publisher("/order", Order, queue_size=1)
+rospy.sleep(2) # Give ros some time to initalize and register the publisher
+
+order_msg = Order()
+# All the objects
+order_msg.objects = ["evergreen", "raspberry", "jetson", "crazyflie", "eraser", "whitebox", "powerbank"]
+# Both tables 
+order_msg.tables = ["table1", "table2"]
+
+order_publisher.publish(order_msg)
